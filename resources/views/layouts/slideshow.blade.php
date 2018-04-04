e5768e8ba9ea32b971574b44efe0c1e50f369c48
Niall Eccles <n.eccles14@gmail.com>
2018-04-04 21:23:07 +0100

    Made slideshow more flexible. Might change from background image to just image.

--- resources/views/layouts/slideshow.blade.php
+++ resources/views/layouts/slideshow.blade.php
@@ -5,21 +5,24 @@
 <div class="slideshow">
     <div id="arrow-left" class="arrow"></div>
     <div id="slider">
-        <div class="slide slide1">
-            <div class="slide-content">
-                <span>Image One</span>
-            </div>
-        </div>
-        <div class="slide slide2">
-            <div class="slide-content">
-                <span>Image Two</span>
-            </div>
-        </div>
-        <div class="slide slide3">
-            <div class="slide-content">
-                <span>Image Three</span>
-            </div>
-        </div>
+        <?php
+            $slide_image = array(
+                "https://images.unsplash.com/photo-1499424271223-6e0ab6be9bd6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=60c285849b19cec273f9212c1c039bee&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb", 
+                "https://images.unsplash.com/photo-1513708726622-aefa716650bb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8662009f4c1407e8b39919d618a4fe7e&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb", 
+                "https://images.unsplash.com/photo-1509914398892-963f53e6e2f1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=dd093986e77ede3c4c6fcaba42fdf544&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb"
+            ); 
+            $i = 0;
+            foreach ($slide_image as $image) { 
+                $i++; 
+            ?>
+                <div class="slide slide<?php echo $i; ?>" style="background:url(<?php echo $image; ?>); background:position:center center; background-repeat:no-repeat; background-size:cover;">
+                    <div class="slide-content">
+                        <span>Image <?php echo $i; ?></span>
+                    </div>
+                </div>
+            <?php 
+            }
+        ?>
     </div>
     <div id="arrow-right" class="arrow"></div>
 </div>
