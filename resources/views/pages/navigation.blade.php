25fd50c1156d1c571d3402f7289c17e687d060a1
Niall Eccles <n.eccles14@gmail.com>
2018-04-06 21:07:24 +0100

    Navigation icon lines are now same length.

--- resources/views/pages/navigation.blade.php
+++ resources/views/pages/navigation.blade.php
@@ -8,9 +8,9 @@
                 <svg class="menu-icon" version="1.1" id="Layer_1" x="0px" y="0px"
                 viewBox="0 0 88.1 88.2" enable-background="new 0 0 88.1 88.2" xml:space="preserve">
                     <g id="XMLID_15_">
-                    <line id="XMLID_18_" fill="none" stroke="#13A8FD" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="76.1" y1="44.1" x2="12.1" y2="44.1"/>
+                    <line id="XMLID_18_" fill="none" stroke="#13A8FD" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="76.1" y1="12.6" x2="12.1" y2="12.6"/>
                     <line id="XMLID_17_" fill="none" stroke="#13A8FD" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="76.1" y1="44.1" x2="12.1" y2="44.1"/>
-                    <line id="XMLID_16_" fill="none" stroke="#13A8FD" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="76.1" y1="44.1" x2="12.1" y2="44.1"/>
+                    <line id="XMLID_16_" fill="none" stroke="#13A8FD" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="76.1" y1="75.6" x2="12.1" y2="75.6"/>
                     </g>
                 </svg>
             </div>
